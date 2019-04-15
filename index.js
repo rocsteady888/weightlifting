e67f8b32e2f7de8e297c1009107069e59644d77a
rocsteady888 <rocsteady888@gmail.com>
2019-04-15 19:02:16 -0400

    added route, models and validation

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const session = require('./routes/api/session');
+const users = require('./server/routes/api/users');
+const profile = require('./server/routes/api/profile');
+const session = require('./server/routes/api/session');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
 // DB Config 
-const db = require('./config/keys').mongoURI;
+const db = require('./server/config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
