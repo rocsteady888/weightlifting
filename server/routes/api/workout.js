3d5c4684da0750d75e3912897d7514ca5ddc224e
rocsteady888 <rocsteady888@gmail.com>
2019-04-19 14:52:47 -0400

    added routes for create, find one, and find recent workouts

--- server/routes/api/workout.js
+++ server/routes/api/workout.js
@@ -14,5 +14,55 @@ const User = require('../../../models/User');
 // @access  Public
 router.get('/test', (req, res) => res.json({ msg: 'Workout Works' }));
 
+// @route   POST api/workout/clockIn
+// @desc    Create a new workout and clock in with a description
+// @access  Private
+router.post('/create', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const workoutFields = {};
+  workoutFields.user = req.user.id;
+  if (req.body.title) workoutFields.title = req.body.title;
+  if (req.body.category) workoutFields.category = req.body.category;
+  if (req.body.subCategory) workoutFields.subCategory = req.body.subCategory;
+  if (req.body.reps) workoutFields.reps = req.body.reps;
+  if (req.body.weight) workoutFields.weight = req.body.weight;
+  if (req.body.rest) workoutFields.rest = req.body.rest;
+
+  new Workout(workoutFields).save()
+    .then(workout => res.json(workout))
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   GET api/workout/find/:workoutId
+// @desc    Find a workout by workout _id
+// @access  Private
+router.get('/find/:workoutId', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+  Workout.findOne({ _id: req.params.workoutId })
+    .then(workouts => {
+      if (!workouts) {
+        errors.noworkout = 'There was no workout found';
+        return res.status(400).json(errors);
+      }
+      res.json(workouts);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route   GET api/workout/recent
+// @desc    Get current user's workouts
+// @access  Private
+router.get('/recent', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const errors = {};
+  Workout.find({ user: req.user.id }).limit(5).sort({ _id: -1 })
+    .then(workouts => {
+      if (!workouts) {
+        errors.workouts = 'There are no workouts for this user';
+        return res.status(400).json(errors);
+      }
+      res.json(workouts);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 
 module.exports = router;
