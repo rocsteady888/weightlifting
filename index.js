e359722a9f6613e57b978b002e2c4ceb59b6ea5e
rocsteady888 <rocsteady888@gmail.com>
2019-04-19 12:18:39 -0400

    added middleware

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
 const users = require('./server/routes/api/users');
 const profile = require('./server/routes/api/profile');
 const session = require('./server/routes/api/session');
+const workout = require('./server/routes/api/workout');
 
 const app = express();
 
@@ -12,21 +15,36 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// DB Config 
-const db = require('./server/config/keys').mongoURI;
+// DB Config
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log('MongoDB Connected'))
+  .then(() => console.log('MongoDB Connected Successfully'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World'));
+// Passport middleware
+app.use(passport.initialize());
 
-// Routes
+// Passport Config
+require('./config/passport')(passport);
+
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/session', session);
+app.use('/api/workout', workout);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
