8ea5801c24bd2e6b01c48c3e02bd633cdbd28da5
rocsteady888 <rocsteady888@gmail.com>
2019-04-20 13:23:01 -0400

    addes Mui themes and localstorage logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import theme from '../src/style/Theme';
+
+import PrivateRouter from './components/common/PrivateRouter';
+
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+import Landing from './components/pages/Landing';
+import Dashboard from './components/pages/Dashboard';
+
+// Check for token
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  setCurrentUser(decoded);
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    console.log('token expired');
+    // Logout user
+    logoutUser();
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <MuiThemeProvider theme={theme}>
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Switch>
+              <PrivateRouter exact path="/dashboard" component={Dashboard} />
+            </Switch>
+          </MuiThemeProvider>
+        </div>
+      </Router>
     );
   }
 }
