7b9f740625644059651d3507f9e63f8f2175789f
rocsteady888 <rocsteady888@gmail.com>
2019-04-20 13:23:45 -0400

    intial commit

---
+++ client/src/style/Theme.js
@@ -0,0 +1,20 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { blue, orange } from '@material-ui/core/colors';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: orange,
+  },
+  status: {
+    danger: 'orange',
+  },
+  typography: {
+    useNextVariants: true,
+  },
+  contrastThreshold: 3,
+  tonalOffset: 0.2,
+});
+
+export default theme;
