0b29d0c79d7e0a49d4d6eda3e1aaf7d8bdca864b
rocsteady888 <rocsteady888@gmail.com>
2019-04-20 13:23:33 -0400

    intial commit

---
+++ client/src/components/pages/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+function Login(props) {
+  const { classes } = props;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12} sm={6}>
+          <form>
+            <input
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              placeholder="Email address"
+              type="email"
+              name="email"
+              required
+            />
+            <input
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              placeholder="Password"
+              type="password"
+              name="password"
+              required
+            />
+            <button type="submit">Submit</button>
+          </form>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
