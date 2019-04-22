b581580847f3d642aa128f67816d76744045f1cf
rocsteady888 <rocsteady888@gmail.com>
2019-04-22 11:08:46 -0400

    converted to functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
-import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './store/actions/authActions';
@@ -7,8 +12,12 @@ import { setCurrentUser, logoutUser } from './store/actions/authActions';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import theme from '../src/style/Theme';
 
-import { Store } from './store/Store';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
+import PrivateRoute from './components/common/PrivateRoute';
+
+import Navbar from './components/navigation/Navbar';
 import Login from './components/pages/Login';
 import Register from './components/pages/Register';
 import Landing from './components/pages/Landing';
@@ -21,32 +30,57 @@ if (localStorage.jwtToken) {
   // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
-  setCurrentUser(decoded);
+  store.dispatch(setCurrentUser(decoded));
 
   // Check for expired token
   const currentTime = Date.now() / 1000;
   if (decoded.exp < currentTime) {
     console.log('token expired');
     // Logout user
-    logoutUser();
+    store.dispatch(logoutUser());
+    // Redirect to login
+    console.log('redirecting');
+    window.location.href = '/login';
   }
 }
 
-function App() {
-  const { state, dispatch } = useContext(Store);
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+function App(props) {
+  const { classes } = props;
+
   return (
-    <Router>
-      <div className="App">
-        {console.log(state)}
-        <MuiThemeProvider theme={theme}>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </MuiThemeProvider>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <MuiThemeProvider theme={theme}>
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </div>
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch>
+          </MuiThemeProvider>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
