da249888c232bc843d7c442b776f8a295ccec2a8
Ryan O'Connor <rocsteady888@gmail.com>
2019-04-25 21:00:38 -0400

    app no longer deals with props or styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
 import { setCurrentUser, logoutUser } from './store/actions/authActions';
@@ -17,11 +13,12 @@ import store from './store/store';
 
 import PrivateRoute from './components/common/PrivateRoute';
 
-import Navbar from './components/navigation/Navbar';
+import SideNav from './components/navigation/SideNav';
 import Login from './components/pages/Login';
 import Register from './components/pages/Register';
 import Landing from './components/pages/Landing';
 import Dashboard from './components/pages/Dashboard';
+import Sessions from './components/pages/Sessions';
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -44,31 +41,20 @@ if (localStorage.jwtToken) {
   }
 }
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-});
-
 function App(props) {
-  const { classes } = props;
 
   return (
     <Provider store={store}>
       <Router>
         <div className="App">
           <MuiThemeProvider theme={theme}>
-            <Navbar />
+            <SideNav />
             <Route exact path="/" component={Landing} />
-            <div className="container">
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
-            </div>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Switch>
+              <PrivateRoute exact path="/sessions" component={Sessions} />
+            </Switch>
             <Switch>
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
@@ -79,8 +65,4 @@ function App(props) {
   );
 }
 
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(App);
+export default App;
