8635882f4392846c6b5d46aacab419e087f6effc
Ryan O'Connor <rocsteady888@gmail.com>
2019-04-29 12:41:51 -0400

    added findOpen sessions

--- client/src/store/actions/sessionActions.js
+++ client/src/store/actions/sessionActions.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 import {
-  CREATE_NEW_SESSION, GET_ERRORS
+  CREATE_NEW_SESSION, 
+  FIND_OPEN_SESSIONS,
+  GET_ERRORS
 } from './types';
 
 export const createNewSession = () => dispatch => {
@@ -9,8 +11,25 @@ export const createNewSession = () => dispatch => {
     .post('api/session/create')
     .then(res =>
       dispatch({
-        type:CREATE_NEW_SESSION,
-        payload:res.data
+        type: CREATE_NEW_SESSION,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+}
+
+export const findOpenSessions = () => dispatch => {
+  axios
+    .get('api/session/findOpen')
+    .then(res =>
+      dispatch({
+        type: FIND_OPEN_SESSIONS,
+        payload: res.data
       })
     )
     .catch(err =>
