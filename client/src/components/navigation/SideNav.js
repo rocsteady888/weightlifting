d0c4604a93591a4272dbcaea8cbd76bbb93a2876
Ryan O'Connor <rocsteady888@gmail.com>
2019-04-29 12:41:20 -0400

    simplified sidenave to only handle toggle drawer

--- client/src/components/navigation/SideNav.js
+++ client/src/components/navigation/SideNav.js
@@ -1,94 +1,35 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { logoutUser } from '../../store/actions/authActions';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import Button from '@material-ui/core/Button';
 
-const drawerWidth = 240;
-
-const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+const styles = {
+  list: {
+    width: 250,
   },
   menuButton: {
-    marginLeft: 12,
+    marginLeft: -12,
     marginRight: 20,
   },
-  hide: {
-    display: 'none',
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  },
-});
+};
 
-class PersistentDrawerLeft extends React.Component {
+class SideNav extends React.Component {
   state = {
-    open: false,
+    left: false,
   };
 
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleDrawerClose = () => {
-    this.setState({ open: false });
+  toggleDrawer = (open) => () => {
+    this.setState({
+      left: open,
+    });
   };
 
   onLogoutClick = (e) => {
@@ -97,95 +38,66 @@ class PersistentDrawerLeft extends React.Component {
   }
 
   render() {
-    const { classes, theme } = this.props;
-    const { open } = this.state;
+    const { classes } = this.props;
     const { isAuthenticated } = this.props.auth;
-
+  
     const authLinks = (
       <React.Fragment>
         <ListItem>
-          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button fullWidth={true} color="inherit" component={Link} to="/">Home</Button>
         </ListItem>
         <ListItem>
-          <Button color="inherit" component={Link} to="/sessions">Sessions</Button>
+          <Button fullWidth={true} color="inherit" component={Link} to="/sessions">Sessions</Button>
         </ListItem>
         <ListItem>
-          <Button color="inherit" component={Link} to="/workout-plan">Plan</Button>
+          <Button fullWidth={true} color="inherit" component={Link} to="/workout-plan">Plan</Button>
         </ListItem>
         <ListItem>
-          <Button color="inherit" component={Link} to="/progress">Progress</Button>
+          <Button fullWidth={true} color="inherit" component={Link} to="/progress">Progress</Button>
         </ListItem>
         <ListItem>
-          <Button color="inherit" onClick={this.onLogoutClick}>Logout</Button>
+          <Button fullWidth={true} color="inherit" onClick={this.onLogoutClick}>Logout</Button>
         </ListItem>
       </React.Fragment>
     );
-
+  
     const guestLinks = (
       <React.Fragment>
         <ListItem>
-          <Button color="inherit" component={Link} to="/">Home</Button>
+          <Button fullWidth={true} color="inherit" component={Link} to="/">Home</Button>
         </ListItem>
         <ListItem>
-          <Button color="inherit" component={Link} to="/login">Login</Button>
+          <Button fullWidth={true} color="inherit" component={Link} to="/login">Login</Button>
         </ListItem>
         <ListItem>
-          <Button color="inherit" component={Link} to="/register">Register</Button>
+          <Button fullWidth={true} color="inherit" component={Link} to="/register">Register</Button>
         </ListItem>
       </React.Fragment>
     );
-
+  
     return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="fixed"
-          className={classNames(classes.appBar, {
-            [classes.appBarShift]: open,
-          })}
-        >
-          <Toolbar disableGutters={!open}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(classes.menuButton, open && classes.hide)}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" color="inherit" noWrap>
-              Weight Training
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          className={classes.drawer}
-          variant="persistent"
-          anchor="left"
-          open={open}
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <div className={classes.drawerHeader}>
-            <IconButton onClick={this.handleDrawerClose}>
-              {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-            </IconButton>
-          </div>
-          <Divider />
-            <List>
-              {isAuthenticated ? authLinks : guestLinks}
-            </List>
-          <Divider />
+      <div>
+        <IconButton className={classes.menuButton} onClick={this.toggleDrawer(true)} color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <Drawer open={this.state.left} onClose={this.toggleDrawer(false)}>
+          <List
+            className={classes.list}
+            tabIndex={0}
+            role="button"
+            onClick={this.toggleDrawer(false)}
+            onKeyDown={this.toggleDrawer(false)}
+          >
+          {isAuthenticated ? authLinks : guestLinks}
+          </List>
         </Drawer>
       </div>
     );
   }
 }
 
-PersistentDrawerLeft.propTypes = {
+SideNav.propTypes = {
   classes: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired,
   logoutUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired
 };
@@ -194,4 +106,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { logoutUser })(withStyles(styles, { withTheme: true })(PersistentDrawerLeft));
+export default connect(mapStateToProps, { logoutUser })(withStyles(styles)(SideNav));
