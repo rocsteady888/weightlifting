3eddc6e305b77aa6bc91a5419d2ae2b8c35da5a8
rocsteady888 <rocsteady888@gmail.com>
2019-05-06 16:48:45 -0400

    added reducers for recent and incomplete sessions

--- client/src/store/reducers/sessionReducer.js
+++ client/src/store/reducers/sessionReducer.js
@@ -1,23 +1,41 @@
-import { 
-  CREATE_NEW_SESSION, 
-  FIND_OPEN_SESSIONS 
+import {
+  CREATE_NEW_SESSION,
+  SESSIONS_LOADING,
+  GET_RECENT_SESSIONS,
+  GET_INCOMPLETE_SESSIONS,
 } from '../actions/types';
 
 const initialState = {
-  currentSession: {}
+  currentSession: {},
+  recentSessions: {},
+  incompleteSessions: {},
+  loading: false
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case SESSIONS_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
     case CREATE_NEW_SESSION:
       return {
         ...state,
-        currentSession: action.payload
+        currentSession: action.payload,
+        loading: false
       };
-    case FIND_OPEN_SESSIONS:
+    case GET_RECENT_SESSIONS:
       return {
         ...state,
-        openSessions: action.payload
+        recentSessions: action.payload,
+        loading: false
+      };
+    case GET_INCOMPLETE_SESSIONS:
+      return {
+        ...state,
+        incompleteSessions: action.payload,
+        loading: false
       };
     default:
       return state;
